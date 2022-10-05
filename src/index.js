7ed35020403fb5da3b508f8906c9a11ecdf8a2c5
Faithful <faithfulokondu@gmail.com>
2022-10-05 20:59:23 +0100

    ...

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { withRouter } from "react-router-dom";
 
 class ScrollToTop extends Component {
   componentDidUpdate(prevProps) {
